cab3266c430c6b479c70207b2fdab732eee91b35
tanay26410 <4646tanaymishra@gmail.com>
2022-10-26 00:17:02 +0530

    added an optimised part

--- fibonacci series.py
+++ fibonacci series.py
@@ -8,3 +8,19 @@ n=int(input())
 print(f(n))
 
 
+'''def f(n):
+        a=0
+        b=1
+        if n<0:
+            return 'negative'
+        if n==0:
+            return a
+        if n==1:
+             return b
+        else:
+             for i in range(2,n+1):
+                  c=a+b
+                  a=b
+                  b=c
+            return c
+'''
